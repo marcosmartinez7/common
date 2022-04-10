ca74c05e08cb8dfd782abbfc023321727f34c63f
Marcos Martinez <mmartinezciompi@gmail.com>
2022-04-10 14:37:00 -0300

    initial

---
+++ src/middlewares/error-handler.ts
@@ -0,0 +1,25 @@
+import { Request, Response, NextFunction } from 'express';
+import { CustomError } from '../errors/custom-error';
+
+export const errorHandler = (
+  error: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  if (error instanceof CustomError) {
+
+    return res.status(error.statusCode).send({
+      errors: error.serializeErrors(),
+    });
+  }
+
+
+  return res.status(500).send({
+    errors: [
+      {
+        message: 'Something went wrong',
+      },
+    ],
+  });
+};
